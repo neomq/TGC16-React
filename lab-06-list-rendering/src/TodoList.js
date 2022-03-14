5830037a65c02d2b881d2d6a7f5695b0238075c2
neomq <neo.minqi@trentglobal.edu.sg>
2022-03-14 16:23:06 +0000

    updated 06-list-rendering

--- lab-06-list-rendering/src/TodoList.js
+++ lab-06-list-rendering/src/TodoList.js
@@ -20,11 +20,27 @@ export default class TodoList extends React.Component {
         return todos;
     }
 
+    renderTodoV2() {
+        // let todos = this.state.tasks.map(function(t){
+        //     return <li>{t}</li>
+        // })
+
+        // let todos = this.state.tasks.map((t)=>{return <li>{t}</li>})
+
+        // let todos = this.state.tasks.map(t=><li>{t}</li>)
+
+        // return todos;
+
+        return this.state.tasks.map( t => <li>{t}</li>)
+    }
+
     render() {
         return (
-            <div>
-                {this.renderTodo()}
-            </div>
+            // <div>{this.renderTodo()}</div>
+            <ul>
+                {/* {this.renderTodoV2()} */}
+                {this.state.tasks.map( t => <li>{t}</li>)}
+            </ul>
         );
     }
 }
